60fdf622f15eacf4b63c7f2fefc364520b88a7af
Ivan <eyeonex.info@gmail.com>
2024-07-31 20:51:03 +0300

    change files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,9 @@ export const Header = () => {
     return (
         <header className='header'>
             <div className='header-content'>
+                <div className="header-logo-container">
+                    <div className='header-logo'>Лого</div>
+                </div>
                 <Nav></Nav>
                 <h1 className='header-title'>Вся жизнь - <span className='header-title-span'>путешествие!</span></h1>
                 <SearchBox />
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import "./SearchBox.css";
 
 export const SearchBox = () => {
-    return (
-        <div className='searchBox-container'>
-            <form className='searchBox-form'>
-                <label htmlFor='from'>
-                    Направление
-                    <input type='text' id='from' className='destinationInput-From' placeholder='Откуда'/>
-                    <input type='text' className='destinationInput-To' placeholder='Куда'/>
-                </label>
-                <label htmlFor='departureDate'>
-                    Дата
-                    <input type='date' id='departureDate' className='destinationInput-date' placeholder='ДД/ММ/ГГ'/>
-                    <input type='date' className='destinationInput-date' placeholder='ДД/ММ/ГГ'/>
-                </label>
-            </form>
-        </div>
-    )
-}
+  return (
+    <div className="searchBox-container">
+      <form className="searchBox-form">
+        <label className='searchBox-formText' htmlFor="from">
+          Направление
+          <div className="searchBox-direction">
+            <input
+              type="text"
+              id="from"
+              className="destinationInput-From searchBox-input"
+              placeholder="Откуда"
+            />
+            <input
+              type="text"
+              className="destinationInput-To searchBox-input"
+              placeholder="Куда"
+            />
+          </div>
+        </label>
+        <label className='searchBox-formText' htmlFor="departureDate">
+          Дата
+          <div className="searchBox-date">
+            <input
+              type="date"
+              id="departureDate"
+              className="destinationInput-date searchBox-input"
+              placeholder="ДД/ММ/ГГ"
+            />
+            <input
+              type="date"
+              className="destinationInput-date searchBox-input"
+              placeholder="ДД/ММ/ГГ"
+            />
+          </div>
+        </label>
+      </form>
+      <button className='searchBox-btn' type='submit'>Найти билеты</button>
+    </div>
+  );
+};
