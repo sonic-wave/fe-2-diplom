d33a24952eeaf3d1c55d1a91a63ec970e1b99ef7
Ivan <eyeonex.info@gmail.com>
2024-10-17 21:46:22 +0300

    selection train

--- src/components/SelectionTrain/SelectionTrain.jsx
+++ src/components/SelectionTrain/SelectionTrain.jsx
@@ -1,3 +1,35 @@
+// import TrainCard from "../TrainCard/TrainCard";
+// import { useSelector } from "react-redux";
+// import "./SelectionTrain.css";
+// import ChangePages from "../../components/ChangePages/ChangePages";
+// import FilterRoute from "../FilterRoute/FilterRoute";
+
+// const SelectionTrain = () => {
+//   const { error } = useSelector((state) => state.trains);
+//   const trains = JSON.parse(localStorage.getItem("trains")) || error;
+//   return (
+//     <>
+//       {trains.length > 0 ? (
+//         <>
+//           <FilterRoute />
+//           <div className="train-cards">
+//             {trains.map((el) => (
+//               <TrainCard card={el} key={el.departure._id} />
+//             ))}
+//           </div>
+//           <ChangePages />
+//         </>
+//       ) : (
+//         <p className="train-cards-error">
+//           Прямых рейсов по маршруту не найдено
+//         </p>
+//       )}
+//     </>
+//   );
+// };
+
+// export default SelectionTrain;
+
 import TrainCard from "../TrainCard/TrainCard";
 import { useSelector } from "react-redux";
 import "./SelectionTrain.css";
@@ -6,10 +38,13 @@ import FilterRoute from "../FilterRoute/FilterRoute";
 
 const SelectionTrain = () => {
   const { error } = useSelector((state) => state.trains);
-  const trains = JSON.parse(localStorage.getItem("trains")) || error;
+  
+  const storedTrains = localStorage.getItem("trains");
+  const trains = storedTrains ? JSON.parse(storedTrains) : error;
+
   return (
     <>
-      {trains.length > 0 ? (
+      {trains && trains.length > 0 ? (
         <>
           <FilterRoute />
           <div className="train-cards">
